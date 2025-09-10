a3530cc9b4f607c46cefaa1bfde67cd80bf22e9a
Nararj <a01714315@tec.mx>
2025-09-10 03:18:21 -0600

    Add files via upload

--- Phasmo_prestige.py
+++ Phasmo_prestige.py
@@ -23,6 +23,33 @@ def calcular_niveles(nivel, prestigio):
         faltante = calculo1 - nivel
         print(f"Tu nivel actual es {actual} y te faltan {faltante} niveles para prestigio deseado")
 
+# Funcion para definir rl rol actual
+
+def roles(nivel):
+    if obtener_nivel(nivel):
+        if nivel >= 1 and nivel <= 99:
+            print("Tu rol es Intern")
+        elif nivel <= 199:
+            print("Tu rol es Recruit")
+        elif nivel <= 299:
+            print("Tu rol es Investigator")
+        elif nivel <= 399:
+            print("Tu rol es Pbt. Investigator")
+        elif nivel <= 499:
+            print("Tu rol es Detective")
+        elif nivel <= 599:
+            print("Tu rol es Technician")
+        elif nivel <= 699:
+            print("Tu rol es Specialist")
+        elif nivel <= 799:
+            print("Tu rol es Analyst")
+        elif nivel <= 899:
+            print("Tu rol es Agent")
+        elif nivel <= 999:
+            print("Tu rol es Operator")
+        elif nivel <= 2000:
+            print("Tu rol es Comissioner")
+
 # Pedir los datos
 
 nivel = int(input("Escribe tu nivel actual: "))
@@ -30,3 +57,4 @@ print("Existe prestigio de 1 a 20")
 prestigio = int(input("Escribe nivel de prestigio deseado: "))
 
 calcular_niveles(nivel, prestigio)
+roles(nivel)
